308a4c43b152c8cb38153b3ce27761a78c15dba6
robertoCastro <bkastro@gmail.com>
2022-11-11 16:59:31 -0300

    Atualização dos parâmetros para atualizar o tempo dos posts

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -5,6 +5,37 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+      name: 'John Fitzpratick',
+      role: 'Front-end developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-11-08 17:30:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://images.unsplash.com/photo-1655703586536-bd2093a25e2b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+      name: 'Melissa Covenet',
+      role: 'Software engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-11-07 17:30:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -13,10 +44,15 @@ export function App() {
 <div className={styles.wrapper}>
     <Sidebar/>
     <main>
-      <Post
-          author="Roberto Castro"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam laboriosam."
-        />
+      { posts.map(post =>{
+        return (
+          <Post
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+          />
+        )
+      }) }
     </main>
 </div>
 
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,32 +1,48 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBr  from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post(){
+export function Post({ author, publishedAt }){
+
+    // Maneira de formatar a data vis js
+    // const publishedDateFormatted = new Intl.DateTimeFormat('pt-br', {
+    //     day: '2-digit',
+    //     month: 'long',
+    //     hour: '2-digit',
+    //     minute: '2-digit'
+    // }).format(publishedAt)
+
+    //Maneira de formatar a data utilizando a biblioteca date-fsn:
+    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm", {
+        locale: ptBr
+    })
+
+    const publishedDateRelativeNow = formatDistanceToNow(publishedAt, {
+        locale:ptBr,
+        addSuffix: true
+    })
+
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://images.unsplash.com/photo-1542740348-39501cd6e2b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Manoela Facchini</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="09 de outubro de 2022 às 09:32" dateTime="2022-10-09 09:32 ">Publicado há 1h</time>
+                <time title={ publishedDateFormatted } dateTime="2022-10-09 09:32 ">
+                    { publishedDateRelativeNow }
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
 
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p><a href="#">👉{' '}jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto</a>{' '}
-                    <a href="#">#nlw</a>{' '}
-                    <a href="#">#rocketseat</a>
-                </p>
             </div>
 
             <form className={styles.commentForm}>
